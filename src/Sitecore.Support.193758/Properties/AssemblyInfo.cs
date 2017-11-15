62ced99a897c7d5287244ad0d68a1baa0cf357f2
DK\AEC <aec@sitecore.net>
2017-11-15 13:17:51 +0200

    commit patch

---
+++ src/Sitecore.Support.193758/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.193758")]
+[assembly: AssemblyProduct("Sitecore.Support.193758")]
+[assembly: ComVisible(false)]
